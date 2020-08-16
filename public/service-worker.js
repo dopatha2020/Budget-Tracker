211773abffaf7dff8b51be942617f9d3d17a6217
Dilshan <dopatha@nc.rr.com>
2020-08-16 15:07:23 -0400

    adding service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,22 @@
+var urlsToCache = [
+    "/",
+    "/db.js",
+    "/index.js",
+    "/manifest.json",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+  ];
+
+var CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(function(cache) {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
+    );
+  });
